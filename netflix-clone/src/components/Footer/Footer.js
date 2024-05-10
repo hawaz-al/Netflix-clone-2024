76e71d83b039f49a605fc2488809d8777392e8b3
haag <haag71421@gmail.com>
2024-05-10 20:30:28 +0300

    header and footer

--- netflix-clone/src/components/Footer/Footer.js
+++ netflix-clone/src/components/Footer/Footer.js
@@ -1,25 +1,24 @@
-import React from "react";
-import FacebookOutlinedIcon from "@mui/icons-material/FacebookOutlined";
-import InstagramIcon from "@mui/icons-material/Instagram";
-import YouTubeIcon from "@mui/icons-material/YouTube";
-import "./Footer.css";
+import React from 'react'
+import FacebookOutlinedIcon from '@mui/icons-material/FacebookOutlined';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import YouTubeIcon from '@mui/icons-material/YouTube';
+import "./footer.css";
 
 const Footer = () => {
   return (
-    <div className="footer_outer_container">
-      <div className="footer_inner_container">
-        <div className="footer_icons">
+    <div className='footer_outer_container'>
+      <div className='footer_inner_container'>
+        <div className='footer_icons'>
           <FacebookOutlinedIcon />
           <InstagramIcon />
           <YouTubeIcon />
         </div>
-
-        <div className="footer_data">
+        <div className='footer_data'>
           <div>
             <ul>
               <li>Audio Description</li>
-              <li> Invester Relations</li>
-              <li> Legal Notices</li>
+              <li>Investor Relations</li>
+              <li>Legal Notice</li>
             </ul>
           </div>
           <div>
@@ -31,9 +30,9 @@ const Footer = () => {
           </div>
           <div>
             <ul>
-              <li>Gift cards</li>
-              <li> Terms of Use</li>
-              <li> Corporate Information</li>
+              <li>Gift Cards</li>
+              <li>Terms of Use</li>
+              <li>Corporate Information</li>
             </ul>
           </div>
           <div>
@@ -44,26 +43,17 @@ const Footer = () => {
             </ul>
           </div>
         </div>
-
-        <div className="service_code">
-          <p>Service Code</p>
+        <div className='service_code'>
+          <p>
+            Service Code
+          </p>
         </div>
-        <div className="copy-write"> &copy; 1997- 2024 Netflix, Inc</div>
+        <div className='copy-write'>
+          &copy; 1997-2024 Netflix, Inc.
+        </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Footer;
-
-// import React from 'react'
-
-// const Footer = () => {
-//   return (
-//     <div>
-
-//     </div>
-//   )
-// }
-
-// export default Footer
+export default Footer
